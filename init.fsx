5395dac34b220a93ee34789a4daeaba2610e590d
Tony Abell <tony@tonyabell.com>
2015-04-19 09:19:30 -0700

    updated cert dll

--- init.fsx
+++ init.fsx
@@ -1,5 +1,9 @@
 #I "packages/Xero.API.SDK/lib/net40"
+#I "packages/System.Security.Cryptography.X509Certificates/lib/net45"
+#I "packages/System.Runtime/lib/net45"
 #r "Xero.Api.dll"
 #r "Xero.Api.Example.Applications.dll"
 #r "Xero.Api.Example.TokenStores.dll"
+#r "System.Security.Cryptography.X509Certificates.dll"
+#r "System.Runtime.dll"
 
